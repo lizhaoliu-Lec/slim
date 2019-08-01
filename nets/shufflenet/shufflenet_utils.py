a504a81593aaca7c25114e51c69b7c0e57514b41
lizhaoliu_Bak <13611425862llz@gmail.com>
2019-08-01 14:26:46 +0800

    fix dynamic spatial shape problem.

--- nets/shufflenet/shufflenet_utils.py
+++ nets/shufflenet/shufflenet_utils.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import six
 import tensorflow as tf
 import tensorflow.contrib.slim as slim
 
@@ -78,7 +79,9 @@ def _channel_shuffle(inputs,
         # group size, depth = g * n
         group_size = depth_in // num_groups
         net = inputs
-        net_shape = net.shape.as_list()
+        net_shape = get_shape_list(net)
+        print(get_shape_list(net), '*** get shape list ***')
+        print(net_shape, '*** shape ***')
         # reshape to (b, h, w, g, n)
         net = tf.reshape(net, net_shape[:3] + [num_groups, group_size])
         # transpose to (b, h, w, n, g)
@@ -86,3 +89,70 @@ def _channel_shuffle(inputs,
         # reshape back to (b, h, w, depth)
         net = tf.reshape(net, net_shape)
         return net
+
+
+def get_shape_list(tensor, expected_rank=None, name=None):
+    """Returns a list of the shape of tensor, preferring static dimensions.
+
+    Args:
+      tensor: A tf.Tensor object to find the shape of.
+      expected_rank: (optional) int. The expected rank of `tensor`. If this is
+        specified and the `tensor` has a different rank, and exception will be
+        thrown.
+      name: Optional name of the tensor for the error message.
+
+    Returns:
+      A list of dimensions of the shape of tensor. All static dimensions will
+      be returned as python integers, and dynamic dimensions will be returned
+      as tf.Tensor scalars.
+    """
+    if name is None:
+        name = tensor.name
+
+    if expected_rank is not None:
+        assert_rank(tensor, expected_rank, name)
+
+    shape = tensor.shape.as_list()
+
+    non_static_indexes = []
+    for (index, dim) in enumerate(shape):
+        if dim is None:
+            non_static_indexes.append(index)
+
+    if not non_static_indexes:
+        return shape
+
+    dyn_shape = tf.shape(tensor)
+    for index in non_static_indexes:
+        shape[index] = dyn_shape[index]
+    return shape
+
+
+def assert_rank(tensor, expected_rank, name=None):
+    """Raises an exception if the tensor rank is not of the expected rank.
+
+    Args:
+      tensor: A tf.Tensor to check the rank of.
+      expected_rank: Python integer or list of integers, expected rank.
+      name: Optional name of the tensor for the error message.
+
+    Raises:
+      ValueError: If the expected shape doesn't match the actual shape.
+    """
+    if name is None:
+        name = tensor.name
+
+    expected_rank_dict = {}
+    if isinstance(expected_rank, six.integer_types):
+        expected_rank_dict[expected_rank] = True
+    else:
+        for x in expected_rank:
+            expected_rank_dict[x] = True
+
+    actual_rank = tensor.shape.ndims
+    if actual_rank not in expected_rank_dict:
+        scope_name = tf.get_variable_scope().name
+        raise ValueError(
+            "For the tensor `%s` in scope `%s`, the actual rank "
+            "`%d` (shape = %s) is not equal to the expected rank `%s`" %
+            (name, scope_name, actual_rank, str(tensor.shape), str(expected_rank)))
